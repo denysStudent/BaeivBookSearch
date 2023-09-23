ae2fe3a6d0ee86c33e5ac3917bc27502b595441c
Denys Baiev <nesfow@gmail.com>
2023-09-23 19:22:05 +1000

    Adding comments

--- BaeivBookSearch/Controllers/BookController.cs
+++ BaeivBookSearch/Controllers/BookController.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿/*
+   DESIGNER: Denys Baiev (ID: 100661798)
+   Date: 20/09/2023   
+   Project Name: Book Search 
+*/
+
+//importing libraries
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,19 +14,25 @@ using BookSearch.Models;
 
 namespace BookSearch.Controllers
 {
+    // Main controller, used to navigate through the pages
     public class BookController : Controller
     {
-        // GET: Book
+        // GET: Book/Index
+        // Controller responsible for the main page
         public ActionResult Index()
         {
             return View();
         }
 
+        // GET: Book/Book
+        // Controller responsible for the book search page
         public ActionResult Book()
         {
             return View();
         }
-        
+
+        // POST: Book/Book
+        // Controller responsible for the book search page with POST request method
         [HttpPost]
         public  ActionResult  Book(string title, string author)
         {
@@ -36,6 +49,7 @@ namespace BookSearch.Controllers
             return View();
         }
 
+        // Method, which is used to send API call to the Google Book API
         public  JsonResult SearchBook()
         {
             BookModel book = new BookModel();
